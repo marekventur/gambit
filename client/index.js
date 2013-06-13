ccb5ed159da32207e7484712544c731e6928481e
Dan Rough <dan.rough@gmail.com>
2013-06-13 23:20:09 +0100

    Back to basics Daniel

--- client/index.js
+++ client/index.js
@@ -1,24 +1,9 @@
 Cards = new Meteor.Collection("cards");
 
 Template.user_loggedout.events({
-	"click #login": function (e, tmpl) {
-		Meteor.loginWithTwitter({
-			requestPermissions: ['screen_name','profile_image_url']
-		}, function(err) {
-				if (err) {
-					alert(err);
-				} else {
-				}
-			});
-	}	
+	"click #login": function(){}
 });
 
 Template.user_loggedin.events({
-	"click #logout": function (e, tmpl) {
-		Meteor.logout(function(err) {
-				if (err) {
-				} else {
-				}
-			});
-	}	
+	"click #logout": {}
 });
--- server/config.js
+++ server/config.js
@@ -1,14 +1,5 @@
 Meteor.startup(function () {
 	Meteor.absoluteUrl('', {replaceLocalhost: true});
-	Accounts.loginServiceConfiguration.remove(
-		{service: "twitter"}	
-	)
 	
-	Accounts.loginServiceConfiguration.insert(
-		{		
-			service: 		"twitter",
-			consumerKey: 	"lqpNeU2AreWVQ052cQ0PCg",
-			secret: 			"5BPqrGKAXVz6e0QIcATw66BU6hkkqOKf3qQpqz5Q"
-		}
-	)
+	
 });
