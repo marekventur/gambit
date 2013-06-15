da485f02db18f37a14185fbbc0de8fb9dc17b909
Marek <mail@marekventur.de>
2013-06-15 16:14:30 +0100

    Use Card objects instead of characters

--- models/players.js
+++ models/players.js
@@ -1,1 +1,9 @@
-Players = new Meteor.Collection("players");
+Players = new Meteor.Collection("players", {
+	// Transform from pure JSON to Objects
+	transform: function(player) {
+		for(var i=0, len=player.cards.length; i < len; i++){
+			player.cards[i] = cardFromJson(player.cards[i]);
+		}
+		return player;
+	}
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -6,77 +6,77 @@ Meteor.startup(function () {
 			gameId: 1, 	
 			name: "Dan",
 			email: "dan@gamevy.com",
-			cards: ["10","J"]
+			cards: [new Card("10", "C"), new Card("J", "C")]
 		});
 		
 		Players.insert({	
 			gameId: 1, 
 			name: "Paul",
 			email: "paul@gamevy.com",
-			cards: ["Q","5"]
+			cards: [new Card("Q", "C"), new Card("5", "C")]
 		});
 		
 		Players.insert({	
 			gameId: 1, 
 			name: "Helen",
 			email: "helen@gamevy.com",
-			cards: ["3","7","2","5"]
+			cards: [new Card("3", "C"), new Card("7", "C"), new Card("2", "C"), new Card("5", "C")]
 		});
 		
 		Players.insert({	
 			gameId: 1,
 			name: "Graham",
 			email: "graham@gamevy.com",
-			cards: ["A","3","9","4"]
+			cards: [new Card("A", "C"), new Card("3", "C"), new Card("9", "C"), new Card("4", "C")]
 		});
 		
 		Players.insert({	
 			gameId: 2,		
 			name: "Bill",
 			email: "bill@gamevy.com",
-			cards: ["10", "A"]
+			cards: [new Card("10", "C"), new Card("A", "C")]
 		});
 		
 		Players.insert({	
 			gameId: 2,		
 			name: "Ben",
 			email: "ben@gamevy.com",
-			cards: ["K", "A"]
+			cards: [new Card("K", "C"), new Card("A", "C")]
 		});
 		
 		Players.insert({	
 			gameId: 3,		
 			name: "Julian",
 			email: "julian@gamevy.com",
-			cards: ["5", "3", "7"]
+			cards: [new Card("5", "C"), new Card("3", "D"), new Card("7", "S")]
 		});
 		
 		Players.insert({	
 			gameId: 3,		
 			name: "Dick",
 			email: "dick@gamevy.com",
-			cards: ["10", "2", "5"]
+			cards: [new Card("10", "H"), new Card("2", "C"), new Card("5", "D")]
 		});
 		
 		Players.insert({	
 			gameId: 3,		
 			name: "George",
 			email: "george@gamevy.com",
-			cards: ["8", "A", "6"]
+			cards: [new Card("8", "D"), new Card("A", "D"), new Card("6", "C")]
 		});
 		
 		Players.insert({	
 			gameId: 3,		
 			name: "Anne",
 			email: "anne@gamevy.com",
-			cards: ["2", "7", "J"]
+			cards: [new Card("2", "S"), new Card("7", "H"), new Card("J", "C")]
 		});
 		
 		Players.insert({	
 			gameId: 3,		
 			name: "Timmy",
 			email: "timmy@gamevy.com",
-			cards: ["Q", "5",]
+			cards: [new Card("Q", "C"), new Card("5", "H")]
 		});
 		
 		Games.insert({
