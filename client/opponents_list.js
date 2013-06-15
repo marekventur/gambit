de8fa9fbf8cbae522c4c4cf028a6e8510aaa2047
Dan Rough <dan.rough@gmail.com>
2013-06-15 10:54:49 +0100

    Replaced array with database collections * Renamed opponents -> players * Removed autopublish package meaning that app now needs to publish/subscribe the data it wishes to use

--- client/opponents_list.js
+++ client/opponents_list.js
@@ -1,21 +1,5 @@
-var opponentsData = [
-	{	
-		userId: "1",
-		userName: "Dan",
-		cards: ["10","J"]
-	},
-	{	
-		userId: "2",
-		userName: "Paul",
-		cards: ["Q","5"]
-	},
-	{	
-		userId: "3",
-		userName: "Helen",
-		cards: ["3","7","2","5"]
-	}
-];
-
 Template.opponents_list.helpers({
-	opponents: opponentsData
+	opponents: function(){
+		return Players.find();	
+	}
 });
